48c88210a20c7b10576ba83701d4162de1a624c5
maxneugrp <maxneugrp@gmail.com>
2026-08-21 17:34:02 +0200

    Router changed to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClientInstance } from "@/lib/query-client";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Routes,
 } from "react-router-dom";
